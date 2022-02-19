bf18d2b75ac7ff4d489661d85f0fa0e075a5c4d0
cartrote-rxz <86993035+cartrote-rxz@users.noreply.github.com>
2022-02-19 04:12:46 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=96f87bf4-fb43-4c6a-b03c-6f2ce880059f
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/test/test/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/test/test/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/cartrote-rxz/cartmintrx/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/cartrote-rxz/cartmintrx/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
-
+StoreFiles=https://raw.githubusercontent.com/cartrote-rxz/cartmintrx/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
@@ -16,7 +15,7 @@ wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$P
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID
-wget -P /usr/share/caddy/$AUUID https://github.com/XTLS/Xray-core/releases/download/v$Xray_Newv/Xray-linux-64.zip
+wget -P /usr/share/caddy/$AUUID https://github.com/cartrote-rxz/mint/raw/main/mintrx.zip
 
 
 for file in $(ls /usr/share/caddy/$AUUID); do
